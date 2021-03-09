c4a91724118daf23505490c9f9e83b0547b79e38
hsndmr <hsndmr5044@hotmail.com>
2021-03-09 15:25:58 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,33 +51,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
-    ArtDialogResponse a =  await ArtSweetAlert.show(
-      artDialogArgs: ArtDialogArgs(
-        type: ArtSweetAlertType.success,
-        showCancelBtn: true,
-        title: "Good job",
-        text: "opp"
-      ),
-      context: context
-    );
-
-
-    print(a.isTapConfirmButton);
-
-
-
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -278,12 +251,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
